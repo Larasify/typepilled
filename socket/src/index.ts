5ac38dc80b397e52e140479b41903c0cba68a059
Eylul Lara Cikis <elcikis1@sheffield.ac.uk>
2023-06-12 11:06:17 +0300

    initial commit of both parts

---
+++ socket/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Application works!');
+});
+
+app.listen(3001, () => {
+  console.log('Application started on port 3001');
+});
