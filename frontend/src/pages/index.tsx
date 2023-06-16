72b1b9a31728bed386788656377d8338d3df2392
Larasify <lara.cikis@gmail.com>
2023-06-16 08:38:54 +0300

    reset button

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -8,7 +8,14 @@ import io from "socket.io-client";
 import { useRouter } from "next/router";
 import Game from "~/components/Game";
 import clsx from "clsx";
-import { FaAt, FaHashtag, FaClock, FaFont, FaQuoteLeft } from "react-icons/fa";
+import {
+  FaAt,
+  FaHashtag,
+  FaClock,
+  FaFont,
+  FaQuoteLeft,
+  FaRedo,
+} from "react-icons/fa";
 import Options from "~/components/Options";
 import { getWords } from "~/utils/getWords";
 import { usePreferenceContext } from "~/context/PreferenceContext";
@@ -47,7 +54,6 @@ const Home: NextPage = () => {
     ref.current?.focus();
   }
 
-
   return (
     <>
       <Head>
@@ -61,17 +67,22 @@ const Home: NextPage = () => {
         <h1 className="h-40 text-4xl font-bold text-primary-color">
           Welcome to Typepilled
         </h1>
-        <Game ref={ref} reset={reset} text={text} time={parseInt(preferences.time)} />
+        <Game
+          ref={ref}
+          reset={reset}
+          text={text}
+          time={parseInt(preferences.time)}
+        />
 
         <div
           className="tooltip tooltip-bottom font-bold"
           data-tip="Restart Test"
         >
           <button
-            className="mt-5 rounded-lg border bg-gray-400 p-1 font-normal"
+            className="mt-5 rounded bg-background-color px-8 py-4 font-normal text-sub-color transition-colors duration-300 hover:text-text-color active:bg-text-color active:text-sub-color"
             onClick={handleClick}
           >
-            Reset
+            <FaRedo />
           </button>
         </div>
       </div>
