1b1eef5e12ef3affa14291010c058922cf4c5a6b
Larasify <lara.cikis@gmail.com>
2023-06-21 15:34:41 +0300

    placeholder about

--- frontend/src/components/Controls.tsx
+++ frontend/src/components/Controls.tsx
@@ -1,15 +1,18 @@
-export function Controls() {
-  return (
-    <div className="font-primary absolute left-0 top-0 flex flex-col items-center justify-end gap-2">
-      <div className="flex items-center space-x-2 font-roboto text-sm text-neutral-500">
-        <kbd className="kbd">tab</kbd>
-        <span className="text-hl"> + </span>
-        <kbd className="kbd">enter</kbd>
-        <span className="text-hl"> or </span>
-        <kbd className="kbd">esc</kbd>
 
-        <span className="text-hl"> - restart test </span>
+const Controls = () =>  {
+  return (
+    <div className="font-primary flex flex-col items-center justify-end gap-2">
+      <div className="flex items-center space-x-2 font-roboto text-sm text-neutral-500">
+        <kbd className="kbd rounded-lg">tab</kbd>
+        <span className="text-hl"> + </span>
+        <kbd className="kbd rounded-lg">enter</kbd>
+        <span className="text-hl"> or </span>
+        <kbd className="kbd rounded-lg">esc</kbd>
+
+        <span className="text-hl "> - restart test </span>
       </div>
     </div>
   );
 }
+
+export default Controls;
--- frontend/src/pages/about.tsx
+++ frontend/src/pages/about.tsx
@@ -1,7 +1,9 @@
+import Controls from "~/components/Controls";
+
 export default function Account() {
   return (
     <div className="flex h-full flex-row justify-around items-center">
-      <div >Hello :3</div> <div>Hi o/</div>
+      <div ><Controls/></div> <div className="text-neutral-500 font-roboto text-lg font-semibold">Hi o/</div>
     </div>
   );
 }
